a07ee75f09299a32e359ecadea10718bbe380915
Doug VonMoser <dvonmoser@cars.com>
2018-12-16 22:03:22 -0600

    day 3

--- day3/index.hs
+++ day3/index.hs
@@ -5,13 +5,29 @@ import qualified Data.Map.Strict as Map
 import Debug.Trace
 
 main = do 
-    input <- readFile "input.txt"
-    print $ foldl (\acc val -> if val > 1 then acc + 1 else acc) 0
-          $ foldl (\acc pair -> Map.insertWith (+) pair 1 acc) Map.empty
-          $ concat
+    input <- readFile "test.txt"
+    print 
+          $ (\x ->  let bigassmap = foldl (\acc pair -> Map.insertWith (+) pair 1 acc) Map.empty $ concat x 
+                    in finishHim 1 bigassmap x)
           $ map plotRects
           $ map parseInput 
           $ lines input
+
+type Point = (Int, Int)
+
+finishHim :: Int -> Map.Map Point Int -> [[Point]] -> Int
+finishHim idx bigassmap (x:xs) = 
+    if allYallOnlyOnes x bigassmap then
+       idx 
+    else 
+        finishHim (idx + 1) bigassmap xs
+
+allYallOnlyOnes :: [Point] -> Map.Map Point Int -> Bool
+allYallOnlyOnes [] _ = True
+allYallOnlyOnes (x:xs) bigassmap = 
+    case Map.lookup x bigassmap of 
+        Just 1 -> allYallOnlyOnes xs bigassmap
+        _ -> False
 
 plotRects :: [Int] -> [(Int, Int)]
 plotRects [_, _, _, _, 0] = [] 
